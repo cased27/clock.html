da65974522d81f611e1c06af8b947ecfa72bd37d
Casey D <caseydasilva@hotmail.com>
2020-06-17 13:49:31 -0700

    tick logic
    added timing logic to each hand
    fixed glitch at end of cycle to remove 'transition' and continue smoothly with each hand of clock (at the 12 time)

---
+++ clock.js
@@ -0,0 +1,32 @@
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+const allHands = document.querySelectorAll(".hand");
+
+function setDate(){
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360)  + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360)  + 90;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360)  + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+    if(secondsDegrees === 90){
+        allHands.forEach(hand => hand.style.transition = 'none');
+        //sets the in-line transition style to NONE
+    } else {
+        allHands.forEach(hand => hand.style.transition = ''); 
+        //resets by removing the above inline style (and therefore reverting to the CSS styles)
+    }
+};
+
+setInterval(setDate, 1000);
+
+//debug: @ 90deg in transition: rotation
